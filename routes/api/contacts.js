0568453c11472256bea75aef3111114a420133f4
Alona Melnykova <alonamelnykova@gmail.com>
2021-12-17 17:53:31 +0300

    init commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,33 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+//==========================================//
+const path = require('path');
+const fs = require('fs').promises;
+const cors = require('cors');
+router.use(cors());
 
+const contactsPath = path.join(__dirname, '../dp/contacts.json');
+//============================================//
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const data = JSON.parse(await fs.readFile(contactsPath, 'utf-8'));
+  res.json(data);
+  console.log(data);
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+module.exports = router;
