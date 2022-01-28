aa69337b86479dfc717e69c6dcee02eebf1d0d73
Alona Melnykova <alonamelnykova@gmail.com>
2022-01-28 15:33:49 +0300

    hw6-verification

--- model/users/index.js
+++ model/users/index.js
@@ -3,8 +3,8 @@ const gravatar = require('gravatar');
 const getUser = async () => Users.find();
 const getUserById = async id => Users.findById(id);
 const getUserByEmail = async email => Users.findOne({ email });
-const addUser = async ({ username, email, password }) => {
-  const newUser = new Users({ username, email });
+const addUser = async ({ username, email, password, verificationToken }) => {
+  const newUser = new Users({ username, email, verificationToken });
 
   newUser.setPassword(password);
   await newUser.updateOne({ avatarURL: gravatar.url(email) });
@@ -16,10 +16,14 @@ const updateToken = async (id, token) => {
   await Users.updateOne({ _id: id }, { token });
 };
 
+const verifyUpdToken = async ({ token }) => {
+  await Users.findOneAndUpdate({ verificationToken: token }, { verify: true });
+};
 module.exports = {
   updateToken,
   getUser,
   getUserById,
   addUser,
   getUserByEmail,
+  verifyUpdToken,
 };
